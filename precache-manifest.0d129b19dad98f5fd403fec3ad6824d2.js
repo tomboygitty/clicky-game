62d4810b9831c1e0f4914d9224cac384e316fba1
Tom Elliott <xenomus@gmail.com>
2019-12-20 21:11:54 -0600

    Updates

---
+++ precache-manifest.0d129b19dad98f5fd403fec3ad6824d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f01f04739c484667ea5c7840052c85",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "fba7178720ab31492780",
+    "url": "/clicky-game/static/css/main.ad76145b.chunk.css"
+  },
+  {
+    "revision": "9f33dda4db52aed8b3fb",
+    "url": "/clicky-game/static/js/2.d0fd21ee.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/clicky-game/static/js/2.d0fd21ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fba7178720ab31492780",
+    "url": "/clicky-game/static/js/main.beba6932.chunk.js"
+  },
+  {
+    "revision": "b859e8c835d6a54c0ef8",
+    "url": "/clicky-game/static/js/runtime-main.bf081868.js"
+  }
+]);
